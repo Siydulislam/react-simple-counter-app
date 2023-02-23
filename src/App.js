4f0ca304b7fc78a743eb59c6920facb43d965536
Siydulislam <siydulislam333@gmail.com>
2023-02-23 10:55:42 +0600

    react state management issuses - lifting state up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
+import Stats from "./components/Stats";
+
+const initialState = [
+  {
+    id: 1,
+    count: 0
+  },
+  {
+    id: 2,
+    count: 0
+  }
+]
 
 export default function App() {
+  const [state, setState] = useState(initialState);
+
+  const totalCount = () => {
+    return state.reduce((total, counter) => total + counter.count, 0);
+  }
+
+  const incrementHandler = (id) => {
+    const updatedCounter = state.map(c => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count + 1
+        }
+      }
+      return { ...c }
+    })
+    setState(updatedCounter);
+  }
+
+  const decrementHandler = (id) => {
+    const updatedCounter = state.map(c => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count - 1
+        }
+      }
+      return { ...c }
+    })
+    setState(updatedCounter);
+  }
 
   return (
     <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
@@ -8,7 +52,8 @@ export default function App() {
         Simple Counter Application
       </h1>
       <div class="max-w-md mx-auto mt-10 space-y-5">
-        <Counter />
+        {state.map(c => <Counter key={c.id} id={c.id} count={c.count} increment={incrementHandler} decrement={decrementHandler} />)}
+        <Stats count={totalCount()} />
       </div>
     </div>
   );
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,15 @@
-import { useState } from "react";
 import Button from "./Button";
 import Count from "./Count";
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
-
-    const incrementHandler = () => {
-        setCount(prevCount => prevCount + 1);
-    }
-
-    const decrementHandler = () => {
-        setCount(prevCount => prevCount - 1);
-    }
-
+export default function Counter({ id, count, increment, decrement }) {
     return (
         <div
             class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
         >
             <Count count={count} />
             <div class="flex space-x-3">
-                <Button handler={incrementHandler}>Increment</Button>
-                <Button handler={decrementHandler}>Decrement</Button>
+                <Button handler={() => increment(id)}>Increment</Button>
+                <Button handler={() => decrement(id)}>Decrement</Button>
             </div>
         </div>
     )
