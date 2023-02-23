9e41f2c1eff135bf28c6659068dc5fce0be04924
Siydulislam <siydulislam333@gmail.com>
2023-02-23 09:50:36 +0600

    implement state management functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,8 @@
-export default function Button({ children }) {
+export default function Button({ children, handler }) {
     return (
         <button
             class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+            onClick={handler}
         >
             {children}
         </button>
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import Button from "./Button";
 import Count from "./Count";
 
 export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    const incrementHandler = () => {
+        setCount(prevCount => prevCount + 1);
+    }
+
+    const decrementHandler = () => {
+        setCount(prevCount => prevCount - 1);
+    }
+
     return (
         <div
             class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
         >
-            <Count />
+            <Count count={count} />
             <div class="flex space-x-3">
-                <Button>Increment</Button>
-                <Button>Decrement</Button>
+                <Button handler={incrementHandler}>Increment</Button>
+                <Button handler={decrementHandler}>Decrement</Button>
             </div>
         </div>
     )
